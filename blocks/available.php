76a106e2ee09975d5549d84989a04674807a709c
alshchetinin <alexsander.qw@gmail.com>
2020-12-09 11:16:43 +0300

    fix bugs

--- blocks/available.php
+++ blocks/available.php
@@ -60,7 +60,11 @@ $form_title = get_field('form_title');
 
                   </div>
             </div>
-            <div class="row" id="flats-price">
+      </div>
+</section>
+<section id="flats-price">
+      <div class="wrapper">
+      <div class="row" >
                   <div class="flats-price" >
                         <h3 class="flats-price__title"> <?php echo $price_title  ?></h3>
                         <?php while( have_rows('kvartiry') ): the_row(); 
